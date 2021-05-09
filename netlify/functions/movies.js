fef4dc93033fcea5b06fffb0392753c59cc18b3f
Dante Capicchioni <dante.capicchioni@kellogg.northwestern.edu>
2021-05-09 14:34:37 -0500

    Final

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -17,48 +17,13 @@ exports.handler = async function(event) {
 
 
   // write the movies to the back-end console, check it out
-  console.log(moviesFromCsv[0])
+  // console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
 // Get the parameters of year and genre
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
-
-// Loop through movie data... for each one:
-// for (let x = 0; x < moviesFromCsv.length; x++) {
-// Store each movie from the csv in memory
-
-let movieResults = {
-  numResults: 0,
-  movies: []
-}
-
-// Run a loop to go through movie data
-for (let i=0; i < moviesFromCsv.length; i++) {
-  // Store each movie in memory
-  let movie = moviesFromCsv[i]
-  // If movie has no genre or runtime ignore results
-  if (movie.genres != `\\N` || movie.runtimeMinutes != `\\N`) {
-
-    } else if (movie.genres == `${genre}` && movie.startYear == `${year}`) {
       
-        // Create a new movie object containing:
-        let movieInfo = {
-          title: movie.primaryTitle,
-          yearReleased: movie.startYear,
-          genre: movie.genres
-          }
-
-        
-
-// Push the movie data object to the final Array
-movieResults.push(movieInfo)
-
-// Count the number of results
-movieResults.numResults = movieResults.movies.count
-
-      
-} else {}
 
   if (year == undefined || genre == undefined) {
     return {
@@ -67,13 +32,45 @@ movieResults.numResults = movieResults.movies.count
     }
   }
   else {
+
+        // Create an empty object to store the num Results and movie data in
+    let movieResults = {
+      numResults: 0,
+      movies: []
+    }
+
+
+    // Run a loop to go through movie data
+    for (let i = 0; i < moviesFromCsv.length; i++) {
+      // Store each movie in memory
+      let movie = moviesFromCsv[i]
+      // If movie has no genre or runtime ignore results
+      if (movie.genres == `\\N` || movie.runtimeMinutes == `\\N`) {
+      } 
+
+      else if (movie.genres == genre && movie.startYear == year) {
+
+        // Create a new movie object containing:
+        let movieInfo = {
+          title: movie.primaryTitle,
+          yearReleased: movie.startYear,
+          genre: movie.genres
+          }
+        // Push the movie data object to the final Array
+        movieResults.movies.push(movieInfo)
+
+      }
+      
+    }
+
+    // Count the number of results
+    movieResults.numResults = movieResults.movies.length
     
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
       body: JSON.stringify(movieResults) // a string of data
     }
-  
-}
-}
+
+  }
 }
